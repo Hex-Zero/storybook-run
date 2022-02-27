b6b21f24b473f513961c463db85d3feccb40f2e1
Maxwell <hex0@live.com>
2022-02-27 21:25:49 +0000

    setup init plop

--- plopfile.ts
+++ plopfile.ts
@@ -1,5 +1,21 @@
 import { NodePlopAPI } from "plop";
 
 export default function (plop: NodePlopAPI) {
-  // plop generator code
+  plop.setGenerator("controller", {
+    description: "application controller logic",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "controller name please",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/{{name}}.js",
+        templateFile: "plop-templates/controller.hbs",
+      },
+    ],
+  });
 }
